48493e312441ba56827154f87635198660f113ce
David Whitlock <alovedalongthe@gmail.com>
2014-06-28 12:06:09 +0700

    Updated tests

--- tests/analysis_test.py
+++ tests/analysis_test.py
@@ -4,42 +4,42 @@ from drat.analysis import Checktext
 
 class TestEmpty(unittest.TestCase):
     def test_no_sentences(self):
-        data = b''
-        sentences = len(re.findall(b'[\.!?]+', data)) or 1
+        data = ''
+        sentences = len(re.findall('[\.!?]+', data)) or 1
         self.assertEqual(sentences, 1)
 
     def test_no_data(self):
         check = Checktext('None.txt', wlist=None, verb=False, web=False)
         with self.assertRaises(SystemExit) as exc:
-            check.run_check(b'')
+            check.run_check('')
         self.assertEqual(exc.exception.code, 1)
 
     def test_raw_data(self):
         check = Checktext('None.txt', wlist=None, verb=False, web=False)
-        result = check.run_check(b'small data')
-        self.assertEqual(result, (0.1, -4.1))
+        result = check.run_check('small data')
+        self.assertEqual(result, (0.0, 7.5))
 
 class TestParrot(unittest.TestCase):
     def setUp(self):
         fname = 'parrot.txt'
-        with open(fname, 'rb') as f:
+        with open(fname, 'r') as f:
             self.data = f.read()
-        self.sentences = len(re.findall(b'[\.!?]+', self.data)) or 1
+        self.sentences = len(re.findall('[\.!?]+', self.data)) or 1
         self.check = Checktext(fname, wlist=None, verb=False, web=True)
 
-    def test_sentences(self):
+    def test_nums(self):
         self.assertEqual(self.sentences, 82)
 
     def test_raw_data(self):
         result = self.check.run_check(self.data)
-        self.assertEqual(result, (7.3, -0.4))
+        self.assertEqual(result, (7.5, 0.7))
 
 class TestGrade1(unittest.TestCase):
     def setUp(self):
         fname = 'frog_toad.txt'
-        with open(fname, 'rb') as f:
+        with open(fname) as f:
             self.data = f.read()
-        self.sentences = len(re.findall(b'[\.!?]+', self.data)) or 1
+        self.sentences = len(re.findall('[\.!?]+', self.data)) or 1
         self.check = Checktext(fname, wlist=None, verb=False, web=True)
 
     def test_sentences(self):
@@ -52,9 +52,9 @@ class TestGrade1(unittest.TestCase):
 class TestGrade5(unittest.TestCase):
     def setUp(self):
         fname = 'amphib.txt'
-        with open(fname, 'rb') as f:
+        with open(fname) as f:
             self.data = f.read()
-        self.sentences = len(re.findall(b'[\.!?]+', self.data)) or 1
+        self.sentences = len(re.findall('[\.!?]+', self.data)) or 1
         self.check = Checktext(fname, wlist=None, verb=False, web=True)
 
     def test_sentences(self):
@@ -62,14 +62,14 @@ class TestGrade5(unittest.TestCase):
 
     def test_raw_data(self):
         result = self.check.run_check(self.data)
-        self.assertEqual(result, (7.3, 6.6))
+        self.assertEqual(result, (7.3, 7.0))
 
 class TestGrade13(unittest.TestCase):
     def setUp(self):
         fname = 'psych_today.txt'
-        with open(fname, 'rb') as f:
+        with open(fname) as f:
             self.data = f.read()
-        self.sentences = len(re.findall(b'[\.!?]+', self.data)) or 1
+        self.sentences = len(re.findall('[\.!?]+', self.data)) or 1
         self.check = Checktext(fname, wlist=None, verb=False, web=True)
 
     def test_sentences(self):
@@ -77,7 +77,7 @@ class TestGrade13(unittest.TestCase):
 
     def test_raw_data(self):
         result = self.check.run_check(self.data)
-        self.assertEqual(result, (8.0, 13.7))
+        self.assertEqual(result, (8.1, 14.6))
 
 if __name__ == '__main__':
     unittest.main()
